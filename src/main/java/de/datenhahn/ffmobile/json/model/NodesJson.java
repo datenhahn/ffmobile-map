948256987373f354ee68e2f416f41721a84abe90
Jonas Hahn <jonas.hahn@ecodia.de>
2015-10-15 01:01:18 +0200

    - try to detect offloaders

--- src/main/java/de/datenhahn/ffmobile/json/model/NodesJson.java
+++ src/main/java/de/datenhahn/ffmobile/json/model/NodesJson.java
@@ -1,5 +1,8 @@
 package de.datenhahn.ffmobile.json.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,6 +11,8 @@ import java.util.Map;
 public class NodesJson {
     Date timestamp;
     String version;
+
+    private static final Logger logger = LoggerFactory.getLogger(NodesJson.class);
     Map<String, FreifunkNode> nodes;
 
     public List<FreifunkNode> getRouters() {
@@ -20,10 +25,26 @@ public class NodesJson {
         return routers;
     }
 
+    private boolean hasWirelessMesh(FreifunkNode node) {
+        if(node.getNodeInfo().getNetwork() != null) {
+            if(node.getNodeInfo().getNetwork().getMesh() != null) {
+                if(node.getNodeInfo().getNetwork().getMesh().get("bat0") != null) {
+                    if(node.getNodeInfo().getNetwork().getMesh().get("bat0").getInterfaces() != null) {
+                        if( node.getNodeInfo().getNetwork().getMesh().get("bat0").getInterfaces().getWireless() != null) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        logger.info("Dropping Non Wireless router: " + node.getNodeInfo().getHostname());
+        return false;
+    }
+
     public List<FreifunkNode> getOnlineRouters() {
         ArrayList<FreifunkNode> routers = new ArrayList<>();
         for(FreifunkNode node : nodes.values()) {
-            if(!node.getFlags().isGateway() && node.getFlags().isOnline()) {
+            if(!node.getFlags().isGateway() && node.getFlags().isOnline() && hasWirelessMesh(node)) {
                 routers.add(node);
             }
         }
